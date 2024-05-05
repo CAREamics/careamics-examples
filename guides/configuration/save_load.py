7119bf5175b194509bff2b24e8222c765b3145f6
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2024-05-05 16:13:32 +0200

    Add save load

---
+++ guides/configuration/save_load.py
@@ -0,0 +1,19 @@
+# %%
+from careamics import save_configuration
+from careamics.config import create_n2v_configuration
+
+config = create_n2v_configuration(
+    experiment_name="Config_to_save",
+    data_type="tiff",
+    axes="ZYX",
+    patch_size=(8, 64, 64),
+    batch_size=8,
+    num_epochs=20,
+)
+save_configuration(config, "config.yml")
+
+
+# %%
+from careamics import load_configuration
+
+config = load_configuration("config.yml")
