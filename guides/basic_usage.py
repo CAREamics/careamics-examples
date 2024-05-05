7903f8c1525e28a85575741a6272e583646fa67c
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2024-05-05 14:45:20 +0200

    Fix basic usage

--- guides/basic_usage.py
+++ guides/basic_usage.py
@@ -6,7 +6,7 @@ from careamics.config import create_n2v_configuration
 # create a configuration
 config = create_n2v_configuration(
     experiment_name="n2v_2D",
-    data_type="tiff",
+    data_type="array",
     axes="YX",
     patch_size=[64, 64],
     batch_size=1,
@@ -17,12 +17,14 @@ config = create_n2v_configuration(
 careamist = CAREamist(config)
 
 # train the model
-train_data = np.random.randint((256, 256))
+train_data = np.random.randint(0, 255, (256, 256))
 careamist.train(train_source=train_data)
 
 # once trained, predict
-pred_data = np.random.randint((128, 128))
+pred_data = np.random.randint(0, 255, (128, 128))
 predction = careamist.predict(source=pred_data)
 
 # export to BMZ format
-careamist.export_to_bmz(path="my_model.bmz", name="N2V 2D")
+careamist.export_to_bmz(
+    path="my_model.bmz", name="N2V 2D", authors=[{"name": "CAREamics authors"}]
+)
